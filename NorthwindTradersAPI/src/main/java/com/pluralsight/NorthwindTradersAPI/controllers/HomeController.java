d7ed9876d3053b4a0e50a8f73cd01659cf41cb64
Brenda Vang <vangsbrenda@gmail.com>
2025-06-17 15:21:41 -0500

    Completing NorthwindTradersAPI exercise

---
+++ NorthwindTradersAPI/src/main/java/com/pluralsight/NorthwindTradersAPI/controllers/HomeController.java
@@ -0,0 +1,21 @@
+package com.pluralsight.NorthwindTradersAPI.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+//    @GetMapping("/")
+//    public String defaultRequest() {
+//        return "Hello World!";
+//    }
+
+
+    @GetMapping("/")
+    public String home(@RequestParam(defaultValue="World") String country) {
+        return "Hello " + country;
+    }
+}
